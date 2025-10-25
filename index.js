4e8c5a1ad396e0e066c8aa33551af9a59f3e214e
Azhan787 <azhanmughal4444@gmail>
2025-10-25 18:55:11 +0500

    Assigment number 4,5

---
+++ index.js
@@ -0,0 +1,59 @@
+// chapter Number : 4
+//Varable Names legal & illegal.
+// Question 1:
+// Declare 3 variables in one line statment.
+
+// var a = 10; b = 20; c = 30;
+//___________________________________________
+//Question 2:
+//Declare 5 ligal & 5 illegal variables names.
+
+// legal variables:-
+
+// 1 name
+// 2 $price
+// 3 _age
+// 4 user1
+// 5 myValue;
+
+//___________________________________________
+
+//illegal variables:-
+
+// 1 1name
+// 2 my-name
+// 3 var.
+// 4 user name
+// 5 @total
+
+//___________________________________________
+
+// Question 3:
+// Display in your browser:-
+//   <h1>Rules for naming JS variables</h1>
+// Variables names can only contain , number ,$ , and_. for example : $my 1st variable
+// Variables must begin with a letter,$ or _.for $name, name or name
+// Variables names are case sensitive
+// Variables names should not be JS keywords
+
+//___________________________________________
+
+
+//Chapter Number : 5
+//Math Expressions.
+//Question 1:
+
+// var num1 = 5
+// var num2 = 10
+// var sum = num1 + num2
+// console.log(sum);
+//Answer{15}
+
+//Question 2:
+
+//subtractions
+ var num1 = 20
+ var num2 = 10
+ var sum = num1 - num2
+ console.log(sum);
+//Answer{10}
