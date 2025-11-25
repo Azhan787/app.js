fda48df0526f96475dc56086498c5831c05db245
Azhan787 <azhanmughal4444@gmail>
2025-11-25 11:27:45 +0500

    functions

---
+++ function.js
@@ -0,0 +1,87 @@
+// //functions
+
+// function name (){
+//     console.log("Azhan")
+// }
+// name()
+// console.log("Azhan")
+// console.log("Azhan")
+// console.log("Azhan")
+// console.log("Azhan")
+// console.log("Azhan")
+// console.log("Azhan")
+// console.log("Azhan")
+
+
+
+// let fnc = () => {
+//     console.log("Azhan")
+// }
+// fnc()
+
+
+// function dance(v1){
+//     console.log(`${v1} KI SHADI`);
+// }
+// dance("AZHAN")
+// dance("ABDULLAH")
+// dance("RAYYAN")
+// dance("HASHIR")
+
+
+// function add (v1,v2){
+//     console.log(v1 + v2)
+// }
+// add(4,6);
+
+// function add (v1=0,v2=0){
+//     console.log(v1 + v2);
+// }
+// add(4,5)
+
+// function abc(...va){
+// console.log(val)
+// }
+// abc(1,2,3,4,5,6,7,8,9,10)
+
+
+// function abc (v){
+//     return 12 + v;
+// }
+// let val = abc(23)
+// console.log(val)
+
+
+// let abc = function(val){
+//     val();
+// }
+// // console.log(abc)
+// abc(function(){
+//     console.log("hello everyone")
+// })
+
+
+
+// function abc(){
+//     let a = 22
+//     return function(){
+        
+//         console.log(a)
+//     }
+// }
+
+
+
+// (function(){
+
+// })
+
+
+
+// abc()
+
+// function abc(){
+//     console.log("hi user")
+// }
+
+
